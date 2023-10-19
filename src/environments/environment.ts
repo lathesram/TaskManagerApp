21b04c43c55ecc4955f109895cbad25a694cb27d
lathes <lathesparan@gmail.com>
2023-10-19 22:44:13 +0530

    Firebase initialized

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'task-management-api-23386',
+    appId: '1:651756322246:web:8fc28d472ccc4a699a06c7',
+    storageBucket: 'task-management-api-23386.appspot.com',
+    apiKey: 'AIzaSyDQuWBcOQPBJcPrsBt2ffhwEX0Vd_FnKDY',
+    authDomain: 'task-management-api-23386.firebaseapp.com',
+    messagingSenderId: '651756322246',
+  },
+};
