3d3c79efebc34fca86463e6e1bb9cb0e56b0175b
lathes <lathesparan@gmail.com>
2023-11-04 21:29:15 +0530

    json updated

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -5,10 +5,19 @@ import { MaterialModule } from '../material/material.module';
 import { ViewTasksComponent } from './view-tasks/view-tasks.component';
 import { TaskRoutingModule } from './task-routing.module';
 import { AddEditDialogComponent } from './add-edit-dialog/add-edit-dialog.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AddEditTaskComponent, ViewTasksComponent, AddEditDialogComponent],
-  imports: [MaterialModule, CommonModule, TaskRoutingModule, ReactiveFormsModule],
+  declarations: [
+    AddEditTaskComponent,
+    ViewTasksComponent,
+    AddEditDialogComponent,
+  ],
+  imports: [
+    MaterialModule,
+    CommonModule,
+    TaskRoutingModule,
+    ReactiveFormsModule,
+  ],
 })
 export class TaskModule {}
